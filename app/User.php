c0d3c096d19db2e15e747b34f573717380d76757
Hesam Bozorgzad <hessam.arbab@gmail.com>
2021-04-07 15:45:11 +0430

    user model rels and funcs

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use App\Role;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function roleIs($type)
+    {
+        return $this->role->is($type);
+    }
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
