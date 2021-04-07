f914a2e417902d9666f94607cf902dfb59f8ad88
Hesam Bozorgzad <hessam.arbab@gmail.com>
2021-04-07 16:19:10 +0430

    add role to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
+use App\Role;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -16,13 +17,15 @@ use Illuminate\Support\Str;
 | model instances for testing / seeding your application's database.
 |
 */
+$rangEnd = Role::count();
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker)use($rangEnd) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'role_id'=> rand(1,$rangEnd)
     ];
 });
