8681ae1137d957317e880363bbd95161b713fde9
Hesam Bozorgzad <hessam.arbab@gmail.com>
2021-04-10 16:27:41 +0430

    order migration

---
+++ database/migrations/2021_04_10_111436_create_orders_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('product_id');
+            $table->foreignId('choice_id');
+            $table->enum('status',['waiting','preparation','ready','delivered']);
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('choice_id')
+                ->references('id')
+                ->on('choices')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
